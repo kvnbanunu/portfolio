6b3d0de7e9084696b9a8ff77805848502554d4fa
kvnbanunu <kvnbanunu@gmail.com>
2025-01-24 13:30:31 -0800

    react starter

---
+++ js/index.jsx
@@ -0,0 +1,26 @@
+const app = document.getElementById("app");
+const root = ReactDOM.createRoot(app);
+
+function LinkButton({ link, label, newTab }) {
+    if ({newTab} == false) {
+        return (
+            <a class="btn btn-primary" href={link} target="_blank">{label}</a>
+        )
+    }
+    return (
+        <a class="btn btn-primary" href={link}>{label}</a>
+    )
+}
+
+function App() {
+    return (
+    <>
+        <h1>This site is being rebuilt with NextJS</h1>
+        <LinkButton link="public/resume_kevin_van_nguyen_kvnbanunu.pdf" label="Resume" newTab="false" />
+        <LinkButton link="https://github.com/kvnbanunu" label="Github" newTab="true" />
+        <LinkButton link="https://www.linkedin.com/in/kvnbanunu/" label="LinkedIn" newTab="false" />
+    </>
+    )
+}
+
+root.render(<App />);
