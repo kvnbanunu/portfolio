fd02881536a316a43ea3d5aff3f4c21e9a418237
kvnbanunu <kvnbanunu@gmail.com>
2025-01-28 14:03:44 -0800

    testing tabs

--- js/index.jsx
+++ js/index.jsx
@@ -2,7 +2,7 @@ const app = document.getElementById("app");
 const root = ReactDOM.createRoot(app);
 
 function LinkButton({ link, label, newTab }) {
-    if (newTab === "false") {
+    if (newTab === "true") {
         return (
             <a className="btn btn-primary" href={link} target="_blank">{label}</a>
         )
@@ -18,7 +18,7 @@ function App() {
         <h1>This site is being rebuilt with NextJS</h1>
         <LinkButton link="public/resume_kevin_van_nguyen_kvnbanunu.pdf" label="Resume" newTab="false" />
         <LinkButton link="https://github.com/kvnbanunu" label="Github" newTab="true" />
-        <LinkButton link="https://www.linkedin.com/in/kvnbanunu/" label="LinkedIn" newTab="false" />
+        <LinkButton link="https://www.linkedin.com/in/kvnbanunu/" label="LinkedIn" newTab="true" />
     </>
     )
 }
