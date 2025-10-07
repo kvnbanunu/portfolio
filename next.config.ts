51fbd48015c600fea8e794dd698e702563177c2b
kvnbanunu <kvnbanunu@gmail.com>
2025-10-06 17:31:23 -0700

    ready script for deployment

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,7 @@ const nextConfig: NextConfig = {
   output: "export",
   images: { unoptimized: true },
   basePath: process.env.NODE_ENV === "production" ? "/portfolio" : "",
-  assetPrefix: process.env.NODE_ENV === "production" ? "/portfolio" : "",
+  assetPrefix: process.env.NODE_ENV === "production" ? "/portfolio/" : "",
 };
 
 export default nextConfig;
