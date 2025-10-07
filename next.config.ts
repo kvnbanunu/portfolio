d849eeb092244787fdb02cc6d600b6a2dec9b7fe
kvnbanunu <kvnbanunu@gmail.com>
2025-10-06 18:56:01 -0700

    updated scripts

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,9 @@
 import type { NextConfig } from "next";
 
-const isGhPages = process.env.DEPLOY_TARGET === "gh-pages";
-const isProd = process.env.NODE_ENV === "production";
-const repo = "/portfolio";
-
 const nextConfig: NextConfig = {
   /* config options here */
   output: "export",
   images: { unoptimized: true },
-  basePath: isGhPages && isProd ? repo : "",
-  assetPrefix: isGhPages && isProd ? `${repo}/` : "",
 };
 
 export default nextConfig;
