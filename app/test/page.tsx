0a6f38a228a749a2472b5dc7aa2781a126b4bc39
kvnbanunu <kvnbanunu@gmail.com>
2026-03-10 09:04:56 -0700

    chore: add test page for colors & components

---
+++ app/test/page.tsx
@@ -0,0 +1,10 @@
+export default function Test() {
+  return (
+    <div className="flex flex-col">
+      <div className="bg-pal1">pal1</div>
+      <div className="bg-pal2">pal2</div>
+      <div className="bg-pal3">pal3</div>
+      <div className="bg-pal4">pal4</div>
+    </div>
+  );
+}
